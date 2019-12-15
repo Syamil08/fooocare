2504ddb695e2e84d00b06f680e7a790247aed408
SYAMIL - <17523109@students.uii.ac.id>
2019-12-15 16:47:08 +0700

    .

--- app/src/main/java/com/example/fooocare/ExampleItem.java
+++ app/src/main/java/com/example/fooocare/ExampleItem.java
@@ -6,6 +6,7 @@ public class ExampleItem {
     private  String judul;
     private  String tanggal;
     private  int position;
+//    Model Agenda
 
     public ExampleItem(String judul, String tanggal, int position) {
         this.judul = judul;
